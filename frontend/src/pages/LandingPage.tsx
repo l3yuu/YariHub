32c78f4ef3202b5d05bbb68aad8894b2caf86993
Lucky <lucky@luckyboba.com>
2026-05-23 01:49:42 +0800

    for PR

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -5,7 +5,7 @@ import HowWeWork from '../components/HowWeWork';
 import Services from '../components/Services';
 
 import Team from '../components/Team';
-import Testimonials from '../components/Testimonials';
+
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import SoftAurora from '../components/SoftAurora';
@@ -41,7 +41,7 @@ const LandingPage = () => {
           <Services />
 
           <Team />
-          <Testimonials />
+
           <Contact />
         </main>
         <Footer />
